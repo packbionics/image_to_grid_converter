d1f8d8f47bc742573a57b082ab46f58f790f99d5
jcbynum2 <jcbynum1@gmail.com>
2022-10-26 18:50:42 -0400

    Edited cmake file to be compatible with current build

--- map_to_jpeg/build/CMakeFiles/map_to_image_node.dir/DependInfo.cmake
+++ map_to_jpeg/build/CMakeFiles/map_to_image_node.dir/DependInfo.cmake
@@ -8,9 +8,17 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEFAULT_RMW_IMPLEMENTATION=rmw_fastrtps_cpp"
+  "RCUTILS_ENABLE_FAULT_INJECTION"
+  "SPDLOG_COMPILED_LIB"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
+  "/opt/ros/foxy/include"
   "/usr/include/opencv4"
   )
 
